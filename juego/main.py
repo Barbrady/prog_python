2c8066ed5fe72c9bd3a6b2a31afad665d696e441
Barbrady <oficial.barbrady@gmail.com>
2016-05-31 17:42:27 +0200

    Actualización

--- juego/main.py
+++ juego/main.py
@@ -1,8 +1,8 @@
 import pygame,sys
 from pygame.locals import *
 
-ALTO = 1024
-ANCHO = 1280
+ALTO = 640
+ANCHO = 800
 
 class Personaje(pygame.sprite.Sprite):
     def __init__(self,fichero):
@@ -12,23 +12,24 @@ class Personaje(pygame.sprite.Sprite):
         self.rectangulo = self.imagen.get_rect()
         self.rectangulo.centerx = 100
         self.rectangulo.centery = 100
+        self.rect = self.rectangulo
         self.velocidad = [0.5, 0.5]
 
     def mover(self, tiempo, teclas):
-        if self.rectangulo.top >= 2:
+        if self.rectangulo.top >= 0:
             if teclas[K_UP]:
                 self.rectangulo.centery -= self.velocidad[1] * tiempo
-        if self.rectangulo.bottom <= ALTO - 2:
+        if self.rectangulo.bottom <= ALTO:
             if teclas[K_DOWN]:
                 self.rectangulo.centery += self.velocidad[1] * tiempo
-        if self.rectangulo.left >= 2:
+        if self.rectangulo.left >= 0:
             if teclas[K_LEFT]:
                 if self.derecha:
                     self.rotar_imagen()
                     self.derecha = False
                     self.izquierda = True
                 self.rectangulo.centerx -= self.velocidad[0] * tiempo
-        if self.rectangulo.right <= ANCHO - 2:
+        if self.rectangulo.right <= ANCHO:
             if teclas[K_RIGHT]:
                 if self.izquierda:
                     self.rotar_imagen()
@@ -37,6 +38,9 @@ class Personaje(pygame.sprite.Sprite):
                 self.rectangulo.centerx += self.velocidad[0] * tiempo
     def rotar_imagen(self):
         self.imagen = pygame.transform.flip(self.imagen,True,False)
+    def situar(self,posicion):
+        self.rectangulo.centerx = posicion[0]
+        self.rectangulo.centery = posicion[1]
 
 
 
@@ -51,7 +55,11 @@ def main():
     pantalla = pygame.display.set_mode((ANCHO,ALTO))
     fondo = cargar_imagen("./imagenes/fondo.png")
     queco = Personaje("./imagenes/queco.png")
+    bomba = Personaje("./imagenes/bomba.png")
+    bomba.situar((300,300))
     clock = pygame.time.Clock()
+    pygame.mixer.music.load("./sound/bso.ogg")
+    pygame.mixer.music.play()
     while True:
         teclas = pygame.key.get_pressed()
         tiempo = clock.tick(30)
@@ -60,7 +68,9 @@ def main():
                 sys.exit(0)
         queco.mover(tiempo,teclas)
         pantalla.blit(fondo,(0,0))
+        pantalla.blit(bomba.imagen,bomba.rectangulo)
         pantalla.blit(queco.imagen,queco.rectangulo)
+        if pygame.sprite.collide_rect(queco,bomba): sys.exit(0)
         pygame.display.flip()
     return 0
 
